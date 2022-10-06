ac7d3fcf7cb51e26664b9eaebbff5df1817c60e4
Jérémy <work@jeremybdn.fr>
2022-10-06 13:16:03 +0200

    🎨 remove interface export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export * from "./interfaces/email-message.interface";
 export * from "./interfaces/mailjet-module-async-options.interface";
 export * from "./interfaces/mailjet-module-options.interface";
 export * from "./mailjet.module";
